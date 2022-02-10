330b3dd3bd69af7f44f0ee8b6a416e1c8a0fbbe3
SubhangiCoder <94190463+SubhangiCoder@users.noreply.github.com>
2022-02-10 21:07:26 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,23 +1,112 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyAqrhgAjY_ARhBW_RbJwWXMttAQ3sXzbLQ",
-    authDomain: "kwitter-b24e3.firebaseapp.com",
-    projectId: "kwitter-b24e3",
-    storageBucket: "kwitter-b24e3.appspot.com",
-    messagingSenderId: "76325781687",
-    appId: "1:76325781687:web:e9429e0ca9114843e3cd32",
-  };
-  firebase.initializeApp(firebaseConfig);
-  function getData() {
-    firebase
-      .database()
-      .ref("/")
-      .on("value", function (snapshot) {
-        document.getElementById("output").innerHTML = "";
-        snapshot.forEach(function (childSnapshot) {
-          childKey = childSnapshot.key;
-          Room_names = childKey;
-          });
+  apiKey: "AIzaSyAqrhgAjY_ARhBW_RbJwWXMttAQ3sXzbLQ",
+  authDomain: "kwitter-b24e3.firebaseapp.com",
+  projectId: "kwitter-b24e3",
+  storageBucket: "kwitter-b24e3.appspot.com",
+  messagingSenderId: "76325781687",
+  appId: "1:76325781687:web:e9429e0ca9114843e3cd32",
+};
+firebase.initializeApp(firebaseConfig);
+
+function getData() {
+  firebase
+    .database()
+    .ref("/")
+    .on("value", function (snapshot) {
+      document.getElementById("output").innerHTML = "";
+      snapshot.forEach(function (childSnapshot) {
+        childKey = childSnapshot.key;
+        Room_names = childKey;
       });
-  }
-  getData();
-  
+    });
+}
+getData();
+
+firebase.initializeApp(firebaseConfig);
+
+user_name = localStorage.getItem("user_name");
+document.getElementById("user_name").innerHTML = "Welcome" + user_name + "!";
+
+function addRoom() {
+  room_name = document.getElementById("room_name").value;
+
+  firebase.database().ref("/").child(room_name).update({
+    purpose: "adding room name"
+  });
+  localStorage.setItem("room_name", room_name);
+  window.location = "kwitter_room.html";
+}
+
+
+function getData() {
+  firebase.database().ref("/").on('value', function (snapshot) {
+    document.getElementById("output").innerHTML = "";
+    snapshot.forEach(function (childSnapshot) {
+      childKey = childSnapshot.key;
+      Room_names = childKey;
+      //Start code
+      console.log("Room Name-" + Room_names);
+      row = "<div class='room_name' id=" + Room_names + "onclick='redirectToRoomName(this.id)' >#" + Room_names + "</div><hr>";
+      document.getElementById("output").innerHTML += row;
+      //End code
+    });
+  });
+}
+getData();
+
+function getData() {
+  firebase.database().ref("/").on('value',
+    function (snapshot) {
+      document.getElementById("output").innerHTML =
+        "";
+      snapshot.forEach(function (childSnapshot) {
+        childKey = childSnapshot.key;
+        Room_names = childKey;
+       //Start code
+        console.log("Room Name-" + Room_names);
+        row = "<div class='room_name' id=" + Room_names + "onclick='redirectToRoomName(this.id)' >#" + Room_names + "</div><hr>";
+        document.getElementById("output").innerHTML += row;
+        //End code
+      });
+    });
+}
+getData();
+
+function redirectToRoomName(name) {
+  console.log(name);
+  localStorage.setItem("room_name", name);
+  window.location = "kwitter_page.html";
+}
+function logout() {
+      localStorage.removeItem("user_name");
+      localStorage.removeItem("room_name");
+      window.location = "kwitter.html";
+}
+firebase.initializeApp(firebaseConfig);
+   user_name = localStorage.getItem("user_name");
+   room_name = localStorage.getItem("room_name");
+
+   function send()
+   {
+         msg=document.getElementById("msg").value;
+         firebase.database().ref(room_name).push({
+               name:user_name,
+               message:msg,
+               like:0
+
+
+         });
+         document.getElementById("msg").value = "";
+   }
+   function getData() { firebase.database().ref("/"+room_name).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
+         firebase_message_id = childKey;
+         message_data = childData;
+
+      } });  }); }
+getData();
+firebase.database().ref(room_name).push({
+  name:user_name,
+  message:msg,
+  like:0
+
+});
